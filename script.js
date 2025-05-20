beca45920f27f957aa0aec5465bd5a5b78094892
PierreLeboeuf <suckstone@gmail.com>
2025-05-20 16:57:27 +0200

    exo 2

---
+++ script.js
@@ -0,0 +1,26 @@
+// // ------------------------------------------
+// EXERCICE 1 : Compter les multiples de 3 ou 5
+// // ------------------------------------------
+
+// let multiples = [];
+
+// for (let nombre = 1; nombre <= 100; nombre++) {
+//     if(nombre % 3 === 0 || nombre % 5 === 0){
+//         multiples.push(nombre)
+//         // console.log(multiples);
+//     }
+//     else {
+//         multiples.push(`!${nombre}`)
+//     }
+   
+// }
+
+// console.log(multiples);
+
+
+// // ------------------------------------------
+// EXERCICE 2 : Filtrer les notes valides
+// // ------------------------------------------
+
+const notes = [15, -3, 12, 0, 19, 22, 8];
+let valides = [];
